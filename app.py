f105a4820891454aebd9acb4e0dd5eb1f827cf73
Charlie <charliemmaher@icloud.com>
2026-07-25 20:11:46 +0100

    change

--- app.py
+++ app.py
@@ -6,7 +6,7 @@ st.write("Welcome to Early Apex Analytics!")
 from backend.Telemetrydashboard  import create_tele_dashboard
 st.title("*Telemetry Dashboard*")
 year = st.selectbox("Year", [2018,2019,2020,2021,2022,2023,2024,2025,2026])
-event = st.selectbox("Race", "Italian Grand Prix", "Spanish Grand Prix" "Belgian Grand Prix")
+event = st.selectbox("Race",[ "Italian Grand Prix", "Spanish Grand Prix" "Belgian Grand Prix"])
 session = st.selectbox("Session", ["Q", "FP1", "R"])
 drivers = st.multiselect("Drivers", ["VER", "HAM", "ALO"])
 if st.button("Generate Telemetry Dashboard"):
